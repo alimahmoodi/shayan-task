45098b4142ab3890dc9863677599d746040d0f75
ali <alimahmudi.alami@gmail.com>
2023-09-27 21:16:58 +0330

    config rtk query and write services

--- src/components/list/card/cardContent/index.tsx
+++ src/components/list/card/cardContent/index.tsx
@@ -1,24 +1,17 @@
+import { UsersList } from "services/endpoints";
 import { Item } from "./item";
-
-interface CardContentProps {
-  fullName: string;
-  sex: string;
-  jobTitle: string;
-  jobType: string;
-  vehicle: string;
-}
 
 export const CardContent = ({
   fullName,
   jobTitle,
   jobType,
-  sex,
+  gender,
   vehicle,
-}: CardContentProps) => {
+}: Omit<UsersList, "id">) => {
   return (
     <>
       <Item title="نام" value={fullName} />
-      <Item title="جنسیت" value={sex} />
+      <Item title="جنسیت" value={gender} />
       <Item title="عنوان شغل" value={jobTitle} />
       <Item title="نوع شغل" value={jobType} />
       <Item title="نوع خودرو" value={vehicle} />
